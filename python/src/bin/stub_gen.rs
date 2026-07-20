a5d9d718e8cb44cd6517b238689cf2bac69ec89c
relf <remi.lafage@onera.fr>
2026-07-20 16:54:35 +0200

    Use rust stub generation for python API stub

--- python/src/bin/stub_gen.rs
+++ python/src/bin/stub_gen.rs
@@ -2,7 +2,16 @@ use pyo3_stub_gen::Result;
 
 fn main() -> Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().filter_or("RUST_LOG", "info")).init();
-    let stub = egobox::stub_info()?;
+    let stub = _rust::stub_info()?;
     stub.generate()?;
+
+    // The Rust stub generator generates a stub file in the python/egobox/_rust directory,
+    // As Python API is the same as core Rust API, we move the stub file to python/egobox/egobox.pyi
+    log::info!("Moving stub file to egobox/egobox.pyi");
+    let from = std::path::Path::new("python/egobox/_rust/__init__.pyi");
+    let to = std::path::Path::new("python/egobox/egobox.pyi");
+    std::fs::rename(from, to).expect(
+        "Should move stub file to egobox/egobox.pyi. Ensure to run this command from the root of the repository, and that the stub file exists.",
+    );
     Ok(())
 }
